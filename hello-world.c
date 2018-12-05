0fb5f596b3c7d14c7ebd66a2ff3f5282ae1894d6
Amarkant Kumar <akumar52@jcp.com>
2018-12-05 10:10:05 +0530

    Print add command line args and args count

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf( "argc = %d\n", argc );
+    for( int i = 0; i < argc; ++i ) {
+        printf( "argv[ %d ] = %s\n", i, argv[ i ] );
+    }
     return 0;
 }
